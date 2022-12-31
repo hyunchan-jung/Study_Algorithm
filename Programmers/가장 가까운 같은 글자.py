11b7da89299a785424141a276d123f1bb6b3c750
hyunchanjung <tmzkdlqmfn1@naver.com>
2022-12-31 22:31:58 +0900

    Create programmers 가장 가까운 같은 글자.py

---
+++ Programmers/가장 가까운 같은 글자.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = []
+    index_map = {}
+    for idx, v in enumerate(s):
+        if v not in index_map:
+            index_map[v] = idx
+            answer.append(-1)
+        else:
+            answer.append(idx - index_map.pop(v))
+            index_map[v] = idx
+
+    return answer
+
+
+assert solution('banana') == [-1, -1, -1, 2, 2, 2]
+assert solution('foobar') == [-1, -1, 1, -1, -1, -1]
